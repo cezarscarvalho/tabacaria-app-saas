8cc70d3cf3e4b8180e2f89b32e7e4af4e32a970b
cezarscarvalho <cezar.souza03@gmail.com>
2026-03-01 16:14:18 -0300

    change_path_supabaseClient

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-undef */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState, useMemo } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '<div styleName={} />
+<utils />supabaseClient';
 import {
     Package, ClipboardList, LogOut, Truck, LayoutDashboard,
     MessageCircle, TrendingUp, RefreshCw, Users, Settings as SettingsIcon,
